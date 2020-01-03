f9c792d08d69cd2a13b198812fb315170e141c94
Ahmed Bilal <ahmed.bilal@netrevelation.com>
2020-01-03 15:58:23 +0500

    Example updated

--- Program.cs
+++ Program.cs
@@ -2,54 +2,42 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Threading;
 
 namespace No_Such_Element_Exception_Handle
 {
-  
 
     class Entrypoint
     {
         static void Main()
         {
 
+            
 
             IWebDriver driver = new ChromeDriver("./");
-            driver.Navigate().GoToUrl("http://116.203.214.214/");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50); // The implicit wait does not work if the element is hidden.
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(4000);
-
-            IWebElement Userid = driver.FindElement(By.Name("email"));
-            Userid.Click();
-            Userid.SendKeys("test@abc.com");
+            // driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Navigate().GoToUrl("https://www.mycabtravel.com");
 
 
-            IWebElement password = driver.FindElement(By.Name("password"));
-            password.Click();
-            password.SendKeys("abc");
-            //Thread.Sleep(3000);
+            IWebElement Flightno = driver.FindElement(By.Id("selectedFlight:input"));
+            Flightno.SendKeys("EK1" + Keys.Enter);
+        
+            IWebElement flightresolve = driver.FindElement(By.Id("flightTickImg"));
 
-            
-            //var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3000));
-            // wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/div/div/div/div/div/div/form/div[5]/btton")));
-
-            IWebElement Button = driver.FindElement(By.XPath("/html/body/div/div/div/div/div/div/form/div[5]/button"));
-            Button.Click();
-
-            if (Button.Displayed)
+            if (flightresolve.Displayed)
 
             {
-
                 System.Console.WriteLine("The element found");
 
             }
 
             else
             {
-
                 System.Console.WriteLine("The element not found");
 
             }
-
             driver.Close();
             driver.Quit();
         }
@@ -57,3 +45,21 @@ namespace No_Such_Element_Exception_Handle
 
     }
 }
+
+//References
+// https://stackoverflow.com/questions/6992993/selenium-c-sharp-webdriver-wait-until-element-is-present
+//https://sqa.stackexchange.com/questions/15327/explicit-wait-is-not-reliable-with-selenium-c
+//https://selenium.dev/docs/site/en/webdriver/waits/
+
+
+
+
+
+
+
+
+
+
+
+
+

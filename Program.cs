3d63112d53b8d9df4f2954638a1e23e92c368ad4
Ahmed Bilal <ahmed.bilal@netrevelation.com>
2020-01-03 12:18:25 +0500

    3rd January commit

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using System;
 
 namespace No_Such_Element_Exception_Handle
 {
+  
+
     class Entrypoint
     {
         static void Main()
@@ -14,7 +16,7 @@ namespace No_Such_Element_Exception_Handle
             IWebDriver driver = new ChromeDriver("./");
             driver.Navigate().GoToUrl("http://116.203.214.214/");
 
-            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(9000);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(4000);
 
             IWebElement Userid = driver.FindElement(By.Name("email"));
             Userid.Click();
@@ -26,8 +28,9 @@ namespace No_Such_Element_Exception_Handle
             password.SendKeys("abc");
             //Thread.Sleep(3000);
 
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3000));
-            //  wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/div/div/div/div/div/div/form/div[5]/btton")));
+            
+            //var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3000));
+            // wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/div/div/div/div/div/div/form/div[5]/btton")));
 
             IWebElement Button = driver.FindElement(By.XPath("/html/body/div/div/div/div/div/div/form/div[5]/button"));
             Button.Click();
@@ -47,30 +50,6 @@ namespace No_Such_Element_Exception_Handle
 
             }
 
-            //IWebElement Button;
-            //try
-            //{
-            //    Button = driver.FindElement(By.XPath("/html/body/div/div/div/div/div/div/form/div[5]/button"));
-            //    // Change the above element address path , then you ll be able to witness the result. Test 
-
-            //    if (Button.Displayed)
-
-            //    {
-
-            //        Messages.Greenmsg("The element found");
-
-            //    }
-
-            //    Button.Click();
-            //}
-
-            //catch (NoSuchElementException)
-            //{
-
-            //    Messages.Redmsg("The element not found");
-
-            //}
-            // Thread.Sleep(3000);
             driver.Close();
             driver.Quit();
         }
